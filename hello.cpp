a88127d0adf65243f603f1a658e0a7a19c0782ad
mdlewis <mdlewis@students.nic.edu>
2023-03-16 10:26:39 -0700

    simple greetings class

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file hello.cpp
+ * @author Matthew Lewis (mdlewis@students.nic)
+ * @brief simple greetings cpp file for hello class
+ * @version 0.1
+ * @date 2023-03-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include"hello.h"
 
 
--- hello.h
+++ hello.h
@@ -1,3 +1,14 @@
+/**
+ * @file hello.h
+ * @author Matthew Lewis (mdlewis@students.nic)
+ * @brief .h file for class hello for lab10
+ * @version 0.1
+ * @date 2023-03-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #ifndef HELLO_H
 #define HELLO_H
 
