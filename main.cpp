2509c940f5e2d3477da1f24b25a1ea6e4f338324
mdlewis <mdlewis@students.nic.edu>
2023-03-16 10:45:46 -0700

    using class to display message

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "hello.h"
+#include <iostream>
+
+
+
+int main()
+{
+    Hello greetings1;
+    std::string phrase = " Oh no I broke github!!!!!";
+
+    greetings1.setGreeting(phrase);
+    std::cout << greetings1.getGreeting() << std::endl;
+}
