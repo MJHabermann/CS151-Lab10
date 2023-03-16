cd8b9fc65b58a30b86789586c2d0d8dd12fd616b
Michael Habermann <MJHabermann@students.nic.edu>
2023-03-16 10:42:06 -0700

    inplemented use for class test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-
+#include "testClass.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
+    test t;
+    t.setTest(21);
+
+    cout << "Hello world from -" << t.getTest() << " pilots!" << endl;
     return 0;
 }
